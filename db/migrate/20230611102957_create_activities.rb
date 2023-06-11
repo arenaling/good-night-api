ad99f4c27577fcd796b45ba182390aba218a1f32
Joni Farizal <arenaling@gmail.com>
2023-06-11 17:31:53 +0700

    prepare model and migration for database tables

---
+++ db/migrate/20230611102957_create_activities.rb
@@ -0,0 +1,11 @@
+class CreateActivities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :activities do |t|
+      t.string :username
+      t.datetime :start_sleep
+      t.datetime :end_sleep
+
+      t.timestamps
+    end
+  end
+end
